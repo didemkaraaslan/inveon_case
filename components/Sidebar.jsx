1f311d3fd4051c9306e807864f252b910aa4a2d9
didem <didem.kucukkaraaslan@altosec.com.tr>
2020-12-02 16:33:24 +0300

    add filters to the sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,53 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from './Checkbox';
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "./Checkbox";
 
 const useStyles = makeStyles((theme) => ({
-  sidebar: {
-    marginTop: theme.spacing(2),
-  },
   sidebarAboutBox: {
     padding: theme.spacing(2),
     backgroundColor: theme.palette.grey[200],
   },
-  sidebarSection: {
-    marginTop: theme.spacing(3),
+  sectionHeader: {
+    marginTop: theme.spacing(2),
+  },
+
+  section: {
+    marginTop: theme.spacing(1),
+    padding: theme.spacing(2),
+    backgroundColor: theme.palette.grey[200],
   },
 }));
 
 const sidebar = {
-  title: 'Filtreler',
-  description: 'Ürünleri renk, beden ve fiyatlarına göre filtreleyin',
-  archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' },
-    { title: 'November 1999', url: '#' },
-    { title: 'October 1999', url: '#' },
-    { title: 'September 1999', url: '#' },
-    { title: 'August 1999', url: '#' },
-    { title: 'July 1999', url: '#' },
-    { title: 'June 1999', url: '#' },
-    { title: 'May 1999', url: '#' },
-    { title: 'April 1999', url: '#' },
-  ],
+  title: "Filtreler",
+  description: "Ürünleri renk, beden ve fiyatlarına göre filtreleyin",
   category: [
-    { key: 'male', name: 'Erkek' },
-    { key: 'female', name: 'Kadın' },
+    { key: "male", name: "Erkek" },
+    { key: "female", name: "Kadın" },
+  ],
+  size: [
+    { key: "s", name: "S" },
+    { key: "m", name: "M" },
+    { key: "l", name: "L" },
+    { key: "xl", name: "XL" },
+  ],
+  color: [
+    { key: "red", name: "Kırmızı" },
+    { key: "green", name: "Yeşil" },
+    { key: "blue", name: "Mavi" },
   ],
 };
 
 const Sidebar = (props) => {
   const classes = useStyles();
 
-  const { title, description, category } = sidebar;
+  const { title, description, category, size, color } = sidebar;
 
   return (
     <Grid item xs={12} md={2}>
@@ -57,18 +58,54 @@ const Sidebar = (props) => {
         </Typography>
         <Typography>{description}</Typography>
       </Paper>
-      <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+
+      {/* Category Filter */}
+      <Typography variant="h6" gutterBottom className={classes.sectionHeader}>
         Kategori
       </Typography>
-      <FormGroup>
-        {category.map((item) => (
-          <FormControlLabel
-            key={item.key}
-            control={<Checkbox defaultChecked name="gilad" />}
-            label={item?.name}
-          />
-        ))}
-      </FormGroup>
+      <Paper elevation={0} className={classes.section}>
+        <FormGroup>
+          {category.map((item) => (
+            <FormControlLabel
+              key={item.key}
+              control={<Checkbox defaultChecked name="gilad" />}
+              label={item?.name}
+            />
+          ))}
+        </FormGroup>
+      </Paper>
+
+      {/* Size Filter */}
+      <Typography variant="h6" gutterBottom className={classes.sectionHeader}>
+        Beden
+      </Typography>
+      <Paper elevation={0} className={classes.section}>
+        <FormGroup>
+          {size.map((item) => (
+            <FormControlLabel
+              key={item.key}
+              control={<Checkbox defaultChecked name="gilad" />}
+              label={item?.name}
+            />
+          ))}
+        </FormGroup>
+      </Paper>
+
+      {/* Color Filter */}
+      <Typography variant="h6" gutterBottom className={classes.sectionHeader}>
+        Renk
+      </Typography>
+      <Paper elevation={0} className={classes.section}>
+        <FormGroup>
+          {color.map((item) => (
+            <FormControlLabel
+              key={item.key}
+              control={<Checkbox defaultChecked name="gilad" />}
+              label={item?.name}
+            />
+          ))}
+        </FormGroup>
+      </Paper>
     </Grid>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { wrapper, State } from '../store/store';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
-import Footer from '../components/Footer';
-import ProductList from '../components/ProductList';
-import { getProducts } from '../store/actions/product';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import * as types from '../store/actions/types';
-const fs = require('fs-extra');
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { wrapper, State } from "../store/store";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import ProductList from "../components/ProductList";
+import { getProducts } from "../store/actions/product";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import * as types from "../store/actions/types";
+const fs = require("fs-extra");
 
 const useStyles = makeStyles((theme) => ({
   mainGrid: {
@@ -44,7 +44,7 @@ export default function Home() {
 
 export const getStaticProps = wrapper.getStaticProps(
   async ({ store, preview }) => {
-    const { products } = await fs.readJson('data/products.json');
+    const { products } = await fs.readJson("data/products.json");
     store.dispatch({ type: types.FETCH_PRODUCTS, payload: products });
   }
 );
