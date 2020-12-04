e50c45767bfc245944ce72cffe64177ce78a0ac5
Ashley20 <kucukkaraaslan.didem@gmail.com>
2020-12-04 12:11:16 +0300

    remove dispatch

--- store/actions/product.js
+++ store/actions/product.js
@@ -1,4 +1,4 @@
-import * as types from "./types";
+import * as types from './types';
 // const fs = require('fs-extra');
 
 // export const getProducts = () => async (dispatch) => {
@@ -9,58 +9,58 @@ import * as types from "./types";
 //   });
 // };
 
-export const addToBasket = (product) => (dispatch) => {
-  dispatch({
+export const addToBasket = (product) => {
+  return {
     type: types.ADD_TO_BASKET,
     payload: product,
-  });
+  };
 };
 
-export const removeFromBasket = (productID) => (dispatch) => {
-  dispatch({
+export const removeFromBasket = (productID) => {
+  return {
     type: types.REMOVE_FROM_BASKET,
     payload: productID,
-  });
+  };
 };
 
-export const addColorFilter = (color) => (dispatch) => {
-  dispatch({
+export const addColorFilter = (color) => {
+  return {
     type: types.ADD_COLOR_FILTER,
     payload: color,
-  });
+  };
 };
 
-export const removeColorFilter = (color) => (dispatch) => {
-  dispatch({
+export const removeColorFilter = (color) => {
+  return {
     type: types.REMOVE_COLOR_FILTER,
     payload: color,
-  });
+  };
 };
 
-export const addSizeFilter = (size) => (dispatch) => {
-  dispatch({
+export const addSizeFilter = (size) => {
+  return {
     type: types.ADD_SIZE_FILTER,
     payload: size,
-  });
+  };
 };
 
-export const removeSizeFilter = (size) => (dispatch) => {
-  dispatch({
+export const removeSizeFilter = (size) => {
+  return {
     type: types.REMOVE_SIZE_FILTER,
     payload: size,
-  });
+  };
 };
 
-export const addCategoryFilter = (category) => (dispatch) => {
-  dispatch({
+export const addCategoryFilter = (category) => {
+  return {
     type: types.ADD_CATEGORY_FILTER,
     payload: category,
-  });
+  };
 };
 
-export const removeCategoryFilter = (category) => (dispatch) => {
-  dispatch({
+export const removeCategoryFilter = (category) => {
+  return {
     type: types.REMOVE_CATEGORY_FILTER,
     payload: category,
-  });
+  };
 };
